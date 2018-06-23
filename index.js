f3ef9214011e4aee36e05370a832d7132e7d7355
Malorie <malorie18@gmail.com>
2018-06-23 19:08:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+function scuberGreetingForFeet (someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  }
+  else if (someValue >= 2000) {
+    if (someValue > 2500) {
+      return 'No can do.'
+    }
+    else {
+    return 'I will gladly take your thirty bucks.'
+    }
+  }
+}
+
+let message;
+function ternaryCheckCity(city) {
+  city === 'NYC' ?  message = 'Ok, sounds good.' :  message = 'No go.'
+  return message
+}
+const trait = 'generous'
+
+let response;
+function switchOnCharmFromTip(trait) {
+  switch (trait) {
+    case 'generous':
+    response = 'Thank you so much.';
+    break;
+    case 'not as generous':
+    response = 'Thank you.';
+    break;
+    default:
+    response = 'Bye.';
+  }
+  return response
+}
